b4cb22f460894407be1a6451d1e45f3383927812
Caio Souza <caio.7s@outlook.com>
2021-04-08 20:49:21 -0300

    feat: Criando estrutura de componente nav-bar

--- Framework-Angular-8-Introduction/course-manager/src/app/app.module.ts
+++ Framework-Angular-8-Introduction/course-manager/src/app/app.module.ts
@@ -6,13 +6,15 @@ import {FormsModule} from "@angular/forms";
 import {CourseListComponent} from "./components/courses/course-list.component";
 import { StarComponent } from './components/star/star.component';
 import { ReplacePipe } from './components/pipe/replace.pipe';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
